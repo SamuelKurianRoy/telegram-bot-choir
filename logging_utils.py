4924f214f674ac1dcca3bbbe19d73e9acbc20cae
SamuelKurianRoy <samuelkurianroy123@gmail.com>
2025-07-24 12:52:09 +0530

    Added Proxy rotation feature to the youtube downloader

--- logging_utils.py
+++ logging_utils.py
@@ -4,30 +4,43 @@
 import logging
 import sys
 
+# Create logger instances
+logger = logging.getLogger("telegram_bot")
+user_logger = logging.getLogger("user_logger")
+downloader_logger = logging.getLogger("downloader_logger")
+
 def setup_loggers():
     """
     Set up and return bot and user loggers with console and file handlers.
     """
     # Bot logger
-    bot_logger = logging.getLogger("telegram_bot")
-    if not bot_logger.handlers:
-        bot_logger.setLevel(logging.INFO)
+    if not logger.handlers:
+        logger.setLevel(logging.INFO)
         # Console handler
         console_handler = logging.StreamHandler(sys.stdout)
         console_handler.setLevel(logging.INFO)
         console_handler.setFormatter(logging.Formatter('%(message)s'))
-        bot_logger.addHandler(console_handler)
+        logger.addHandler(console_handler)
         # File handler
         file_handler = logging.FileHandler("bot_log.txt", encoding='utf-8')
         file_handler.setLevel(logging.INFO)
         file_handler.setFormatter(logging.Formatter('%(asctime)s - %(message)s'))
-        bot_logger.addHandler(file_handler)
+        logger.addHandler(file_handler)
+
     # User logger
-    user_logger = logging.getLogger("user_logger")
     if not user_logger.handlers:
         user_logger.setLevel(logging.INFO)
         user_logger.propagate = False
         user_handler = logging.FileHandler("user_log.txt", encoding='utf-8')
         user_handler.setFormatter(logging.Formatter("%(asctime)s - %(message)s"))
         user_logger.addHandler(user_handler)
-    return bot_logger, user_logger 
+    
+    # Downloader logger
+    if not downloader_logger.handlers:
+        downloader_logger.setLevel(logging.INFO)
+        downloader_logger.propagate = False
+        downloader_handler = logging.FileHandler("downloader_log.txt", encoding='utf-8')
+        downloader_handler.setFormatter(logging.Formatter("%(asctime)s - %(message)s"))
+        downloader_logger.addHandler(downloader_handler)
+
+    return logger, user_logger
